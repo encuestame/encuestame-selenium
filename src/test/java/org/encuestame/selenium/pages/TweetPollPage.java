a952bb8a12bdb41b237b9036be0630c0f090d50c
Diana Morales Urbina <dianmorales@gmail.com>
2015-06-24 19:00:00 +0200

    Updated Tweepoll page class with new methods to find elements.

--- src/test/java/org/encuestame/selenium/pages/TweetPollPage.java
+++ src/test/java/org/encuestame/selenium/pages/TweetPollPage.java
@@ -1,68 +1,46 @@
 package org.encuestame.selenium.pages;
 
-import junit.framework.TestCase;
-import org.encuestame.selenium.AbstractSelenium;
-import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.net.URL;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 /**
- * Created by dmorales on 5/26/15.
+ * Tweetpoll Page.
+ * @author Morales, Diana Paola paolaATencuestame.org
+ * @since May 26, 2015
  */
-public class TweetPollPage extends AbstractPages {
+public class TweetPollPage extends AbstractEnmePages {
 
     /**
-     * Test Home Popular Hashtags
+     * Home Popular Hashtags
      */
-    @Test
     public void testHomePopularHashtags(){
-        WebElement signin_home = driver.findElement(By.id("signin-home"));
-        Assert.assertNotNull(signin_home);
-        signin_home.click();
+        clickElementById("signin-home");
         // Input
-        WebElement username = driver.findElement(By.id("j_username"));
-        Assert.assertNotNull(username);
-        WebElement password = driver.findElement(By.id("j_password"));
-        Assert.assertNotNull(password);
-        //
-        username.sendKeys("dmorales");
-        password.sendKeys("XSLFLD");
-        WebElement signInbutton = driver.findElement(By.id("signin-button"));
-        Assert.assertNotNull(signInbutton);
-        signInbutton.click();
+        setInputFieldValue("j_username", "dmorales");
+        setInputFieldValue("j_password", "XSLFLD");
+
+        clickElementById("signin-button");
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
         driver.get("http://localhost:8080/encuestame/user/dashboard");
-
-        WebElement add_gadget_button = driver.findElement(By.id("add_gadget_button"));
-        Assert.assertNotNull(add_gadget_button);
+        verifyElementByIdOnPage("add_gadget_button");
         // Click on TweetPoll tab
-        driver.findElement(By.linkText("TweetPolls")).click();
+        clickByLinkText("TweetPolls");
         this.createTweetPoll();
     }
 
     /**
-     * Test Create Tweetpoll.
+     * Create Tweetpoll.
      */
-    @Test
     public void createTweetPoll() {
-        WebElement create = driver.findElement(By.id("create_tweetpoll_button"));
-        create.click();
+        clickElementById("create_tweetpoll_button");
+
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         WebElement element31=  driver.findElement(By.id("question"));
@@ -71,20 +49,17 @@ public class TweetPollPage extends AbstractPages {
 
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-        driver.findElement(By.id("answerAddText")).sendKeys("Y");
-        driver.findElement(By.id("answerAddButton_label")).click();
+        setInputFieldValue("answerAddText", "Y");
+        clickElementById("answerAddButton_label");
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         //driver.findElement(By.id("answerAddText")).sendKeys("N");
         //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.findElement(By.id("dijit_form_TextBox_1")).sendKeys("hashtag");
-        driver.findElement(By.id("suggestButton_dijit__WidgetsInTemplateMixin_6_label")).click();
+        setInputFieldValue("dijit_form_TextBox_1","hashtag");
+        clickElementById("suggestButton_dijit__WidgetsInTemplateMixin_6_label");
 
-         WebElement schedule =
-                driver.findElement(By.id("schedule"));
-        Assert.assertNotNull(schedule);
-        schedule.click();
+        clickElementById("schedule");
 
        /* WebElement myDynamicElement = (new WebDriverWait(driver, 20))
                  .until(ExpectedConditions.presenceOfElementLocated(By.id("scheduleTime")));*/
@@ -92,9 +67,7 @@ public class TweetPollPage extends AbstractPages {
        /* WebDriverWait wait = new WebDriverWait(driver, 10);
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("scheduledTime")));
 */
-        WebElement schedule1 = driver.findElement(By.id("scheduledTime"));
-
-        Assert.assertNotNull(schedule1);
+        verifyElementByIdOnPage("scheduledTime");
        // new Select(driver.findElement(By.id("scheduledTime"))).selectByIndex(2);
 
         Select oSelection = new Select(driver.findElement(By.id("scheduledTime")));
